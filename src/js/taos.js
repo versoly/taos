6a16d3944223ef4863af52b86f61bdf7cb220a82
Regan Karlewicz <regan@tvlabs.ai>
2024-08-28 14:09:40 -0700

    Remove new line

--- src/js/taos.js
+++ src/js/taos.js
@@ -83,7 +83,6 @@
   const observer = new MutationObserver(mutations => {
     mutations.forEach(({target}) => {
       const classes = (target && target.getAttribute('class')) || "";
-
       if (classes && !classes.includes('taos-init') && classes.includes('taos:')) {
         elements.push(initElement(target))
       }
